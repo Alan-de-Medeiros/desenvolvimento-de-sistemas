9454fa65c55e5ddc1fb873cedf2c9b56ca66a830
ALAN DE MEDEIROS <alan_de-medeiros@estudante.sesisenai.org.br>
2025-03-13 19:05:43 -0300

    semana06

--- semana06/src/app/page.tsx
+++ semana06/src/app/page.tsx
@@ -1,38 +1,35 @@
-import Link from "next/link";
+'use client'
+
+
+import { useState } from "react";
 import "./home.css";
-import Person from "@/components/Person";
 
 export default function Home() {
-  const lista = [
-    {
-      name: "Gustavo",
-      age: 27
-    },
-    {
-      name: "Maria",
-      age: 20
-    },
-    {
-      name: "João",
-      age: 40
-    }
-  ]
+  //let contador = 0;
+  const [contador, setContador] = useState<number>(0);
+  
 
+  function incrementar() {
+    setContador(contador + 1);
+  }
+
+  function resetar() {
+    setContador(0)
+  }
+
+  function decrementar() {
+    setContador(contador - 1);
+  }
+
+  
+  
 
   return (
     <div>
-      <h1>Hello Worlds</h1>
-      <h2>teste</h2>
-
-      <Link href="/login">
-        <button>LOGIN</button>
-      </Link>
-
-      <div className="container">
-        {lista.map((item) => (
-          < Person nome={item.name} idade={item.age} key={item.name} />
-        ))}
-      </div>
+      <h1>Contador: {contador}</h1>
+      <button onClick={incrementar}>INCREMENTAR</button>
+      <button onClick={resetar}>RESETAR</button>
+      <button onClick={decrementar}>DECREMENTAR</button>
     </div>
   );
 }
